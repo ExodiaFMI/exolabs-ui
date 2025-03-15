6cd763591c0652fe98fd6c400fce482a61423b7d
Valentin Ratchev <valentinratchev@gmail.com>
2025-03-15 13:54:00 +0200

    Remove layout from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import './App.css';
-import Layout from './layout/layout';
 import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
-  return (
-    <Layout>
-      <AppRoutes />
-    </Layout>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
