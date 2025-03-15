15d9bcf640d18452f1b001b960d59b6c647a0ac9
Genadi Kolev <genadimkolev@gmail.com>
2025-03-15 16:28:30 +0200

    feature(dashboard-to-BE): add tanstack query dependancy

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { Route, BrowserRouter as Router, Routes } from 'react-router';
+import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
-import PrivateRoute from './PrivateRoute';
-import Dashboard from '../pages/Dashboard';
 import StudyTheoryMode from '../pages/study/StudyTheoryMode';
-import Layout from '../layout/layout';
+import PrivateRoute from './PrivateRoute';
 
 const AppRoutes = () => {
   return (
@@ -13,14 +12,6 @@ const AppRoutes = () => {
         <Route path="/" element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/study/:courseId" element={<StudyTheoryMode />} />
-          <Route
-            path="/dashboard"
-            element={
-              <Layout>
-                <Dashboard />
-              </Layout>
-            }
-          />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
