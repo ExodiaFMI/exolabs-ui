82bc774f31460a7dcaaf8ac54d857f7f9f5f02de
Genadi Kolev <genadimkolev@gmail.com>
2025-03-15 02:58:48 +0200

    feature(dashboard-page): add progress bar to course card

--- src/components/user-dashboard/CourseCard.tsx
+++ src/components/user-dashboard/CourseCard.tsx
@@ -1,19 +1,23 @@
 import { FC } from 'react';
+import Progress from '../../shared/components/Progress';
 
 interface CourseCardProps {
   title: string;
-  description?: string;
+  description: string;
+  progress: number;
 }
 
-const CourseCard: FC<CourseCardProps> = ({ title, description }) => {
+const CourseCard: FC<CourseCardProps> = ({ title, description, progress }) => {
   return (
     <article className="flex flex-col gap-5 bg-secondary text-light p-4 rounded-lg shadow-lg">
       <img className="h-35 w-full bg-primary rounded-lg" />
 
       <div>
         <h2 className="text-xl font-bold mb-1">{title}</h2>
-        <p className="text-sm">{description ?? ''}</p>
+        <p className="text-sm">{description}</p>
       </div>
+
+      <Progress progress={progress} />
     </article>
   );
 };
--- src/pages/UserDashboard.tsx
+++ src/pages/UserDashboard.tsx
@@ -6,26 +6,31 @@ const cards = [
     id: 1,
     title: 'React Basics',
     message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    progress: 50,
   },
   {
     id: 2,
     title: 'React Basics',
     message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    progress: 70,
   },
   {
     id: 3,
     title: 'React Basics',
     message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    progress: 30,
   },
   {
     id: 4,
     title: 'React Basics',
     message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    progress: 90,
   },
   {
     id: 5,
     title: 'React Basics',
     message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    progress: 10,
   },
 ];
 
@@ -34,7 +39,12 @@ const UserDashboard: FC = () => {
     <section className="text-dark p-6">
       <div className="grid grid-cols-3 gap-10">
         {cards.map(card => (
-          <CourseCard key={card.id} title={card.title} description={card.message} />
+          <CourseCard
+            key={card.id}
+            progress={card.progress}
+            title={card.title}
+            description={card.message}
+          />
         ))}
       </div>
     </section>
