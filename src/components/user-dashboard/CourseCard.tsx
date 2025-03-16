94d910399e11f630d0d56ff380cf5114b5cbf1eb
Valentin Ratchev <valentinratchev@gmail.com>
2025-03-16 15:26:33 +0200

    Add courses banners

--- src/components/user-dashboard/CourseCard.tsx
+++ src/components/user-dashboard/CourseCard.tsx
@@ -5,12 +5,13 @@ interface CourseCardProps {
   title: string;
   description: string;
   progress: number;
+  imageSrc: string;
 }
 
-const CourseCard: FC<CourseCardProps> = ({ title, description, progress }) => {
+const CourseCard: FC<CourseCardProps> = ({ title, description, progress, imageSrc }) => {
   return (
     <article className="flex flex-col justify-between h-full gap-5 bg-secondary text-light p-4 rounded-lg shadow-lg">
-      <img className="h-35 w-full bg-primary rounded-lg" />
+      <img className="h-35 w-full bg-primary rounded-lg object-cover " src={imageSrc} />
 
       <div>
         <h2 className="text-xl font-bold mb-1">{title}</h2>
